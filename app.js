6d334a56be533af66af395a087202093407915ec
Adam-Collier <adamcollierart@gmail.com>
2017-12-10 23:31:07 +0000

    changed redirect route for github authentication failure

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.post('/signup', [
 ], users.signupPost)
 
 app.get('/auth/github', passport.authenticate('github', { scope: ['user:email profile repo'] }));
-app.get('/auth/github/callback', passport.authenticate('github', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/github/callback', passport.authenticate('github', { successRedirect: '/', failureRedirect: '/' }));
 
 app.get('/logout', users.logout);
 

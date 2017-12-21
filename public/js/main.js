29c96c6205b1899700581b7c5e8b6cb0368e78c1
Adam-Collier <adamcollierart@gmail.com>
2017-12-21 19:04:10 +0000

    removed fetchAPI, created function to show and hide readme

--- public/js/main.js
+++ public/js/main.js
@@ -10,65 +10,6 @@ var app = (function () {
     }
   }
 
-  var fetchAPI = () => {
-    fetch('/api').then(
-      (response) => response.json()
-    )
-      .then((projects) => {
-        projects.forEach(function (x) {
-          var projectCard = `
-          <div class="project">
-            <div>
-              <div>
-                <h2 class="name">${x.repo}</h2>
-                <p class="username">${x.username}</p>
-                <a href="${x.repoUrl}" class="repo">Github repo</a>
-              </div>
-              <div>
-                <p class="level">${x.difficulty}</p>
-              </div>
-            </div>
-            <div>
-              <h4>comment</h4>
-              <p class="comment">${x.comment}</p>
-            </div>
-            <div>
-              <div>
-                <p class="readme">README</p>
-                <?xml version="1.0" standalone="no"?>
-                <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-                <svg width="100%" height="100%" viewBox="0 0 5 6" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                  xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
-                  <path d="M4.154,2.505l-4.154,2.506l0,-5.011l4.154,2.505Z" style="fill:#000;" />
-                </svg>
-              </div>
-              <p class="languages">${x.languages}</p>
-            </div>
-            <div class="github-readme">
-              ${x.README}
-            </div>
-          </div>
-        `
-          document.querySelector('.projects').insertAdjacentHTML('beforeend', projectCard);
-        })
-      })
-      .then(() => {
-        document.querySelectorAll('.project > div:nth-of-type(3)').forEach(function (x) {
-          x.addEventListener('click', function () {
-            var readme = this.parentElement.parentElement;
-            this.querySelector('svg').classList.toggle('arrow')
-
-            if (this.nextElementSibling.style.maxHeight) {
-              this.nextElementSibling.style.maxHeight = null;
-            } else {
-              this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + "px";
-            }
-          })
-        })
-      })
-      .catch((err) => document.querySelector('.projects').innerHTML = "api not responding")
-  }
-
   var inputFocus = (login) => {
     setTimeout(() => {
       login.parentElement.querySelector('input#email').focus();
@@ -99,6 +40,17 @@ var app = (function () {
         }
         xhr.send("url=" + input.value);
       }
-    }, 250)
-  return { fetchAPI, inputFocus, checkRepoExists };
+    }, 250);
+
+    var showReadme = (readmeClick) => {
+      readmeClick.querySelector('svg').classList.toggle('arrow');
+      let readmeDiv = readmeClick.nextElementSibling;
+      if (readmeDiv.style.maxHeight) {
+        readmeDiv.style.maxHeight = null;
+      } else {
+        readmeDiv.style.maxHeight = readmeDiv.scrollHeight + "px";
+      }
+    }
+
+  return { inputFocus, checkRepoExists, showReadme };
 })();
